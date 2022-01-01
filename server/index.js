7af63bf3e950c48691c7bba9829639eed387de09
MbuguaGeorge <mbuguag026@gmail.com>
2022-01-01 20:21:25 +0300

    implementing socket.io on server side to show connection and disconnection

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = 5000;
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('we have a onnection');
+
+    socket.on('disconnect', () => {
+        console.log('user has left')
+    })
+});
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
