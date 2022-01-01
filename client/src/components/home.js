29ddbcd70fe7d43c7c67baaf00b96214f2739126
MbuguaGeorge <mbuguag026@gmail.com>
2022-01-01 19:45:41 +0300

    installing client side and server side dependancies :)

---
+++ client/src/components/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Hey</h1>
+        </div>
+    )
+}
---
+++ client/src/components/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chat from './chat'
+import Home from './home'
+import {Routes, Route} from 'react-router-dom'
+
+const Main = () => (
+    <Routes>
+        <Route exact path = "/" element={<Home/>} />
+        <Route  path = "/chat" element={<Chat/>} />
+    </Routes>
+)
+
+export default Main;
